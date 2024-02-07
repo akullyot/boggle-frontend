1b153917f652399b1755d9a05d45638838f4d314
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2024-02-06 21:19:39 -0600

    added to the homepage a test fetch to the server

--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -1,5 +1,5 @@
 //Import all required hooks and dependencies
-import { useContext, useState }   from "react"
+import { useContext, useState, useEffect }   from "react"
 import { useNavigate }            from "react-router-dom"
 import { CurrentUser }            from "../../Contexts/CurrentUser"
 import { HashLink }               from "react-router-hash-link";
@@ -9,12 +9,26 @@ import { HashLink }               from "react-router-hash-link";
 export default function Home () {
     //WE REQUIRE A TOAST FOR WHEN A PERSON ABANDONS A GAME, read from the url for now but thats not a good method
     //need to check if the value is true or error
-    
+    const [data, setData] = useState(null)
+    useEffect(() => {
+        const serverFetchTest = async () => {
+            const response = await fetch(`https://boggle-backend-413ebb7ed653.herokuapp.com/api`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            setData(data.message);
+        }
+        serverFetchTest();
+    })
     
 
     return(
         <main>
             <h1> Welcome to Boggle</h1>
+            {data}
         </main>
     )
 }
